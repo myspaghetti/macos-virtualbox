1689e11b004e080e932695b37d9bbc08f9e0e79d
img2tab <31696646+img2tab@users.noreply.github.com>
2019-03-06 04:15:51 +0000

    incremented version number

--- macos-guest-virtualbox.sh
+++ macos-guest-virtualbox.sh
@@ -2,7 +2,7 @@
 # One-key semi-automatic installer of macOS on VirtualBox
 # (c) img2tab, licensed under GPL2.0 or higher
 # url: https://github.com/img2tab/macos-guest-virtualbox
-# version 0.38.1
+# version 0.39
 
 # Requirements: 33.5GB available storage on host
 # Dependencies: bash>=4.0, unzip, wget, dmg2img,
