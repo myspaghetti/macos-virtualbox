dccaf672b0f51ae48944ee757e3773b90c64df61
Jack <31696646+myspaghetti@users.noreply.github.com>
2020-01-31 14:18:39 +0200

    incremented version number

--- macos-guest-virtualbox.sh
+++ macos-guest-virtualbox.sh
@@ -2,7 +2,7 @@
 # Semi-automatic installer of macOS on VirtualBox
 # (c) myspaghetti, licensed under GPL2.0 or higher
 # url: https://github.com/myspaghetti/macos-guest-virtualbox
-# version 0.85.3
+# version 0.85.4
 
 # Requirements: 40GB available storage on host
 # Dependencies: bash >= 4.3, xxd, gzip, unzip, wget, dmg2img,
