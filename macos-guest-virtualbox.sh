ca827185d479cd0325488db0d3ce7fdc9c546fea
TheBotlyNoob <72417807+TheBotlyNoob@users.noreply.github.com>
2021-02-23 08:27:49 -0500

    updated version because I think its reasonable

--- macos-guest-virtualbox.sh
+++ macos-guest-virtualbox.sh
@@ -2,7 +2,7 @@
 # Push-button installer of macOS on VirtualBox
 # (c) myspaghetti, licensed under GPL2.0 or higher
 # url: https://github.com/myspaghetti/macos-virtualbox
-# version 0.98.3
+# version 0.98.4
 
 #       Dependencies: bash  coreutils  gzip  unzip  wget  xxd  dmg2img
 #  Optional features: tesseract-ocr  tesseract-ocr-eng
